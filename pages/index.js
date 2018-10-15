5f56f7b2f298efb18d79aa516a23bbd6239e313a
fbjerko <f.bjerko@gmail.com>
2018-10-15 10:14:51 +0200

    dasdsa

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React,  { Component } from "react";
+
+class Index extends Component {
+
+    render() {
+        return(
+<div>
+    <h1>HELLO REACT / NEXT.JS </h1>
+</div>
+        );
+    }
+}
+
+export default Index;
