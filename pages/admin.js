f2b8167c7572463af08f2c41299e5d3a8b45dc3a
fbjerko <f.bjerko@gmail.com>
2018-10-19 13:49:46 +0200

    dsa

--- pages/admin.js
+++ pages/admin.js
@@ -3,6 +3,7 @@ import LayoutGlobal from "../components/LayoutGlobal";
 import IndexReturn from "../components/IndexReturn";
 import NavBar from "../components/NavBar";
 import { Router } from "../routes";
+import Players from "./admin/players";
 
 
 
@@ -10,7 +11,7 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       
+       players: false
     };
   }
 
@@ -20,16 +21,13 @@ class Dashboard extends Component {
     return (
       <div>
   <LayoutGlobal />
-    
-       
-        <NavBar/>
         
         <div className="container">
         <h1>Dashboard</h1>
        
 
         <div className="btn-admin-nav">
-              <button className="btn-nav" onClick={() => Router.pushRoute("/admin/players")}>
+              <button className="btn-nav" id="btn-players" onClick={() => Router.pushRoute("/admin/players")}>
                 Players
               </button>
 
@@ -41,11 +39,16 @@ class Dashboard extends Component {
                 Teams
               </button>
 
+            </div>
+
+             <div className="btn-admin-nav-bottom">
+              
+
                <button className="btn-nav" onClick={() => Router.pushRoute("/admin/matches")}>
                 Matches
               </button>
 
-              <button className="btn-nav" onClick={() => Router.pushRoute("/admin/stadiums")}>
+              <button className="btn-nav"  id="btn-stadium" onClick={() => Router.pushRoute("/admin/stadiums")}>
                 Stadiums
               </button>
 
