76b0d1e6bab03edc6efe67ec10602c60bedc7b78
fbjerko <f.bjerko@gmail.com>
2018-10-22 10:17:17 +0200

    Fix

--- components/admin-player/ListPlayers.js
+++ components/admin-player/ListPlayers.js
@@ -1,17 +1,46 @@
-import React, {Component} from "react";
-
+import React, { Component } from "react";
 
 class ListPlayers extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      players: []
+      players: [],
+      ready: false
     };
   }
 
+  async componentDidMount() {
+    try {
+      const response = await fetch(`http://localhost:5000/api/players/all`);
+      const json = await response.json();
+      this.setState({
+        players: json,
+        ready: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
-    return <div>Heihei</div>;
+    if (this.state.ready === false) {
+      return (
+        <div>
+          Heihei
+          <br />
+          Vi venter på info
+        </div>
+      );
+    } else {
+        return (
+            <div>
+              Her er info
+              <br />
+              {this.state.players}
+            </div>
+          );
+    }
   }
 }
 
