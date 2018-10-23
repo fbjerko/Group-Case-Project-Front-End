ae82c6e79e27f0568262c08a1f558493672280f9
fbjerko <f.bjerko@gmail.com>
2018-10-23 10:13:57 +0200

    o

--- components/admin-player/ListPlayers.js
+++ components/admin-player/ListPlayers.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PlayerInfo from "./PlayerInfo";
 
 class ListPlayers extends Component {
   constructor(props) {
@@ -6,14 +7,37 @@ class ListPlayers extends Component {
 
     this.state = {
       players: [],
-      ready: false
+      ready: false,
+      activePlayer: "",
+      displayPlayer: false
     };
+
+    this.showPlayer = this.showPlayer.bind(this);
+    this.closePlayer = this.closePlayer.bind(this);
+  }
+
+  showPlayer(id) {
+    console.log("huhuhuhu" + id);
+    this.setState({
+      activePlayer: id,
+      displayPlayer: true
+    });
+  }
+
+  closePlayer() {
+      this.setState({
+          activePlayer: "",
+          displayPlayer: false
+      })
   }
 
   async componentDidMount() {
+    // "await fetch(`http://localhost:5000/api/user/all`);"
+    console.log("Hey");
     try {
-      const response = await fetch(`http://localhost:5000/api/players/all`);
-      const json = await response.json();
+      const response = await fetch(`http://localhost:5000/api/player/all`);
+        const json = await response.json();
+      console.log(json);
       this.setState({
         players: json,
         ready: true
@@ -24,6 +48,19 @@ class ListPlayers extends Component {
   }
 
   render() {
+    const players = this.state.players.map(player => {
+      return (
+        <li key={player.playerId}>
+          <button
+            className="btn-admin-player"
+            onClick={() => this.showPlayer(player.playerId)}
+          >
+            {player.playerId + " "}
+          </button>
+        </li>
+      );
+    });
+
     if (this.state.ready === false) {
       return (
         <div>
@@ -32,16 +69,41 @@ class ListPlayers extends Component {
           Vi venter på info
         </div>
       );
+    } else if (this.state.displayPlayer === true) {
+      return (
+        <div>
+          <div>
+            <PlayerInfo id={this.state.activePlayer} />
+          </div>
+          <button
+            className="btn-admin-player"
+            onClick={this.closePlayer}
+          >
+           Back
+          </button>
+        </div>
+      );
     } else {
-        return (
-            <div>
-              Her er info
-              <br />
-              {this.state.players}
-            </div>
-          );
+      return (
+        <div>
+            <br/>
+          <ul>{players}</ul>
+          <br />
+        </div>
+      );
     }
   }
 }
 
 export default ListPlayers;
+
+/*
+
+
+ onClick={this.showPlayer}
+   
+
+
+
+     
+      */
