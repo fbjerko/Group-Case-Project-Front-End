7db757ce2d67334bfb7ee39b182dadd361e4c618
Tonjez <tonjeeth@hotmail.com>
2018-10-26 13:25:37 +0200

    dap

--- components/forms/goalsForm.js
+++ components/forms/goalsForm.js
@@ -1,35 +1,68 @@
 import React from "react";
 
-function sendGoal() {
-  
-    var xhttp = new XMLHttpRequest();
-  
-    xhttp.open("POST", "http://localhost:5000/api/user", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(
-      JSON.stringify({
-        type: document.getElementById("goal_type").value
-      })
-    );
-  
-    
-  }
 
-const goalsForm = () => (
-<div className="info-container">
-      
-      <div className="seasons-container">
-      <div className="top">
-        <h2>Create goal type</h2>
-      </div>
-       <p>Goal type </p>
-       <input type="text" placeholder="Write a goal type" id="goal_type"/>
-       <br></br>
-       <br></br>
-       <input className="btn-index" type="button" value="Submit"></input>
-      </div>
+class GoalForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name:''
+          }
+    }
 
-    </div>
-)
 
-export default goalsForm;
+    updateInput = (event)=>{
+        if(event.target.id=='name'){
+            this.setState({name:event.target.value});
+        }
+
+    }
+
+    sendGoal = ()=>{
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST", "http://localhost:5000/api/goal", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(
+            JSON.stringify({
+                name:this.state.name
+            })
+        );
+        xhttp.onreadystatechange = ()=>{
+            if (xhttp.readyState == XMLHttpRequest.DONE) {
+                if(xhttp.status==201){
+                    console.log("Created");
+                }else if (xhttp.status==403){
+                    console.log("Failed to create")
+                }
+            }
+        }
+    }
+
+
+
+    render(){
+        return(<div className="info-container">
+
+            <div className="seasons-container">
+                <div className="top">
+                    <h2>Create a goal</h2>
+                </div>
+                <p>Goal type</p>
+                <input onChange={this.updateInput} value={this.state.name} type="text" placeholder="Write a goal type" id="name" />
+                <br></br>
+                <br></br>
+                <input className="btn-index" type="button" value="Submit" onClick={this.sendGoal}></input>
+            </div>
+
+        </div>
+        );
+
+    }
+
+
+}
+
+
+
+export default GoalForm;
